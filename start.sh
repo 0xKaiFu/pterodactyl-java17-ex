18816e1fe4ddaef97b9dd8a7463180e333837a75
0xKaiFu <82840369+0xKaiFu@users.noreply.github.com>
2024-02-15 03:15:10 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#Changed by KaiFu for better Compatability with the Console... (Removed the screen and tmux commands)
+cd "$(dirname "$(readlink -fn "$0")")" || exit 1
+
+# check if java is installed
+if [ -x "$(command -v java)" ]; then
+  # if screen is present use that
+  # this check is elevated as tmux is sometimes present by default
+  if [ -x "$(command -v screen)" ]; then
+    # DO NOT CHANGE THE SUPPLIED MEMORY HERE. THIS HAS NO EFFECT ON THE NODE INSTANCE. USE THE launcher.cnl INSTEAD
+    java -Xms128M -Xmx128M -XX:+UseZGC -XX:+PerfDisableSharedMem -XX:+DisableExplicitGC -jar launcher.jar
+  elif [ -x "$(command -v tmux)" ]; then
+    # DO NOT CHANGE THE SUPPLIED MEMORY HERE. THIS HAS NO EFFECT ON THE NODE INSTANCE. USE THE launcher.cnl INSTEAD
+    java -Xms128M -Xmx128M -XX:+UseZGC -XX:+PerfDisableSharedMem -XX:+DisableExplicitGC -jar launcher.jar
+  else
+    echo "No screen or tmux installation found, you need to install at least one of them to run CloudNet"
+    exit 1
+  fi
+else
+  echo "No valid java installation was found, please install java in order to run CloudNet"
+  exit 1
+fi
